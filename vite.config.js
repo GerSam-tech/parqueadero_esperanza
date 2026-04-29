bee79a244ea315d0f229df29199f8187b39f2a00
Skywalk3rcode <mr7475371@gmail.com>
2026-04-29 16:43:05 -0500

    guardando cambios antes de cambiar a gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/parqueadero_esperanza/",
-  server: {
-    host: true, // permite acceso desde red/ngrok
-    allowedHosts: [
-      "antler-overstate-earthy.ngrok-free.dev"
-    ]
-  }
-});
+  base: '/parqueadero_esperanza/'
+})
